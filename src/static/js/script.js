8af4ea1a9acbcb0118cdf085b0985b91142711d0
JoshGoA <josugoar@gmail.com>
2020-01-26 16:15:00 +0100

    Refactored bounding box renderer

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,14 +1,12 @@
-var cnvs = document.getElementsByTagName("canvas");
-var rndr_ctx = cnvs[0].getContext("2d");
-var skct_ctx = cnvs[1].getContext("2d");
+var cnv = document.getElementById("sketch");
+var ctx = cnv.getContext("2d");
 var position = { x: 0, y: 0 };
 // Resize canvas
-function setSize(cnvs) {
-  for (let i = 0; i < cnvs.length; i++) {
-    cnvs[i].width = window.innerWidth;
-    cnvs[i].height = window.innerHeight;
-    cnvs[i].getContext("2d").clearRect(0, 0, cnvs[i].width, cnvs[i].height);
-  }
+function setSize() {
+  cnv.width = window.innerWidth;
+  cnv.height = window.innerHeight;
+  ctx.clearRect(0, 0, cnv.width, cnv.height);
+  $(".box-wrapper").html("");
 }
 // Locate mouse
 function setPosition(event) {
@@ -22,19 +20,19 @@ function draw(event) {
   }
   if (event.buttons === 1) {
     if (mode === "brush") {
-      skct_ctx.lineWidth = 10;
-      skct_ctx.globalCompositeOperation="source-over";
+      ctx.lineWidth = 10;
+      ctx.globalCompositeOperation="source-over";
     } else {
-      skct_ctx.lineWidth = 20;
-      skct_ctx.globalCompositeOperation="destination-out";
+      ctx.lineWidth = 20;
+      ctx.globalCompositeOperation="destination-out";
     }
-    skct_ctx.beginPath();
-    skct_ctx.lineCap = "round";
-    skct_ctx.strokeStyle = "white";
-    skct_ctx.moveTo(position.x, position.y);
+    ctx.beginPath();
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "white";
+    ctx.moveTo(position.x, position.y);
     setPosition(event);
-    skct_ctx.lineTo(position.x, position.y);
-    skct_ctx.stroke();
+    ctx.lineTo(position.x, position.y);
+    ctx.stroke();
   }
 }
 
@@ -42,7 +40,7 @@ var compression_ratio = 10;
 // Send post request
 function post() {
   $(".load-wrapper").fadeIn(500);
-  img = cnvs[1].toDataURL("image/jpeg", 1 / compression_ratio).split(",")[1];
+  img = cnv.toDataURL("image/jpeg", 1 / compression_ratio).split(",")[1];
   $.ajax({
     type: "POST",
     contentType: "application/json;charset=utf-8",
@@ -50,22 +48,21 @@ function post() {
     url: "/model/predict/",
     data: JSON.stringify({
       "image": img,
-      "show": true,
+      "show": false,
       "save": false
     }),
     dataType: "json",
     success: function (ret) {
       console.log(ret);
       if (ret) {
+        var block = "";
         for (let i = 0; i < ret.length; i++) {
-          rndr_ctx.lineWidth = 3;
-          rndr_ctx.strokeStyle = "black";
-          rndr_ctx.beginPath();
-          rndr_ctx.rect(ret[i].x, ret[i].y, ret[i].width, ret[i].height);
-          rndr_ctx.stroke();
-          rndr_ctx.font = "30px Arial";
-          rndr_ctx.fillText(ret[i].pred, ret[i].x, ret[i].y - 5);
+          block +=
+            "<div class='box' style='--width: "+ ret[i].width + "px; --height: " + ret[i].height + "px; --margin-top: " + (ret[i].y - 3) + "px; --margin-left: " + (ret[i].x - 3) + "px;'>" +
+              "<div class='label'>" + ret[i].pred + "</div>" +
+            "</div>";
         }
+        $(".box-wrapper").html(block);
       }
     },
     error: function () {
